2b4457e130a843739107754642eaa0da1beb0b11
yaodongcheng <yaodongcheng@example.com>
2026-06-07 08:18:15 +0800

    删除没必要的aiinfluence

--- ExampleModVS/ExampleMod/ExampleMod/AIInfluenceProject/AIInfluence/@@c-hH'5~V6qSRJ-ipgt-W.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-// Token: 0x020000B2 RID: 178
-internal class @@c/hH'5~V6qSRJ|ipgt-W : Attribute
-{
-}
--- ExampleModVS/ExampleMod/ExampleMod/AIInfluenceProject/AIInfluence/cRpyY-uXi0{vSYUTMNGZ!Z7-(.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-
-// Token: 0x02000014 RID: 20
-[CompilerGenerated]
-[cRpyY\uXi0{vSYUTMNGZ!Z7*(]
-internal sealed class cRpyY\uXi0{vSYUTMNGZ!Z7*( : Attribute
-{
-}
